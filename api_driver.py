84f507efe84689b92d4491a1ca34091bcf3c48c2
Szymon Rogowski <szymon0r0official@gmail.com>
2023-04-15 02:24:18 +0200

    Api finished

--- api_driver.py
+++ api_driver.py
@@ -31,13 +31,13 @@ def predict():
         heuristic_acc = classifier.get_accu_simple_heuristic()
 
         sample_row = pd.Series({
-            'Elevation': sample_pred[0],
-            'Slope': sample_pred[1],
-            'Aspect': sample_pred[2],
-            'Hillshade_Noon': sample_pred[7],
-            'Wilderness_Area_3': sample_pred[12],
-            'Horizontal_Distance_To_Hydrology': sample_pred[3],
-            'Hillshade_9am': sample_pred[6]
+            'Elevation': int(sample_pred[0]),
+            'Slope': int(sample_pred[2]),
+            'Aspect': int(sample_pred[1]),
+            'Hillshade_Noon': int(sample_pred[7]),
+            'Wilderness_Area_3': int(sample_pred[12]),
+            'Horizontal_Distance_To_Hydrology': int(sample_pred[3]),
+            'Hillshade_9am': int(sample_pred[6])
         })
 
         predicted_cover_type = classifier.get_pred_simple_heuristic(sample_row)
@@ -64,9 +64,9 @@ def predict():
         classifier.outliers()
         classifier.split()
         classifier.scaling()
-        classifier.create_model()
-        ann_acc = classifier.train()
-        predicted_cover_type = classifier.predict_cover_type(predict)
+        classifier.create_model(optimizer="adam", hidden_layer_size=128, epochs=1, dropout_rate=0.0, batch_size=32, activation="relu")
+        ann_acc = classifier.train(epochs=1, batch_size=32)
+        predicted_cover_type = classifier.predict_cover_type(sample_pred)
         predicted_cover_type = np.int64(predicted_cover_type).tolist()
         output_json = {"ANN Accuracy": ann_acc,
                        "Predict 'Cover_type' value for sample - NN": predicted_cover_type}
